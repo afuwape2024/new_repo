f65d0ce029164588986cb6c913d3579ac47dfdb4
afuwape2024 <afuwape2024@gmail.com>
2026-01-07 02:18:56 +0000

    third commit

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id = aws_subnet.public_subnet
+  subnet_id = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -74,9 +74,143 @@ resource "aws_route_table" "private_rt" {
 }
 
 resource "aws_route_table_association" "private_rt_assoc" {
-  subnet_id = aws_subnet.private_subnet
+  subnet_id = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_network_acl" "public_nacl" {
+  vpc_id = aws_vpc.justvpc.id
+}
+
+resource "aws_network_acl_rule" "public_nacl_rule" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = aws_vpc.justvpc.cidr_block
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "public2_nacl_rule" {
+  network_acl_id = aws_network_acl.public_nacl.id
+  rule_number    = 200
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = aws_vpc.justvpc.cidr_block
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_association" "public_nacl_assoc" {
+  subnet_id = aws_subnet.public_subnet.id
+  network_acl_id = aws_network_acl.public_nacl.id
+}
+#================================================================
+
+resource "aws_network_acl" "private_nacl" {
+  vpc_id = aws_vpc.justvpc.id
+}
+
+resource "aws_network_acl_rule" "private_nacl_rule" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = aws_vpc.justvpc.cidr_block
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_association" "private_nacl_assoc" {
+  subnet_id = aws_subnet.private_subnet.id
+  network_acl_id = aws_network_acl.private_nacl.id
+}
+
+#================================================================
+resource "aws_security_group" "public_instance_sg" {
+  name        = "instance_sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.justvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#================================================================
+resource "aws_security_group" "private_instance_sg" {
+  name        = "private_instance_sg"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.justvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "private_instance_sg"
+  }
+}
+
+#connecting the nat gateway to the public subnet
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+} 
+resource "aws_route_table" "private_rt_with_nat" {
+  vpc_id = aws_vpc.justvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"  
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "private_rt_with_nat"
+  }   
+}
+
+
+
 
 
 
